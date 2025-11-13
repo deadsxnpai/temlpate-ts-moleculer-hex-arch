d47049e6707241cd9a202e012c59b8f75623ab02
deadsxnpai <deadsxnpai@gmail.com>
2025-11-13 17:04:31 +0300

    fix: imports fix

--- src/infrastructure/adapters/input/api/graphql/template.controller.ts
+++ src/infrastructure/adapters/input/api/graphql/template.controller.ts
@@ -1,5 +1,5 @@
-import { ITemplateController } from '../../../../../domain/ports/input/api/template.controller';
-import { ITemplateRepository } from '../../../../../domain/ports/output/template.repository';
+import { ITemplateController } from '@/domain/ports/input/api/template.controller';
+import { ITemplateRepository } from '@/domain/ports/output/template.repository';
 
 export class TemplateController implements ITemplateController {
 	constructor(private repository: ITemplateRepository) {}
