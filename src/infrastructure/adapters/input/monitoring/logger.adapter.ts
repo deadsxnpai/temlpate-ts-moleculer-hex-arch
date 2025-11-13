fe6a2e0d4013b86d49988bef05149d2a2d4e8ac7
deadsxnpai <deadsxnpai@gmail.com>
2025-11-13 17:08:12 +0300

    feat: added basic loger logic

--- src/infrastructure/adapters/input/monitoring/logger.adapter.ts
+++ src/infrastructure/adapters/input/monitoring/logger.adapter.ts
@@ -1,1 +1,7 @@
-export {};
+import { ILogger } from '@/domain/ports/input/monitoring/logger';
+
+export class Logger implements ILogger {
+	log(): Promise<void> {
+		throw new Error('Method not implemented.');
+	}
+}
